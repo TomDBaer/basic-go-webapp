fd042a893ef7f7389655fbca802541ba9cb2c9ca
Tom Baer <thomasbaehren@gmail.com>
2024-08-28 21:06:05 +0200

    added a divide route

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"net/http"
 )
@@ -26,10 +27,35 @@ func addValues(x, y int) int {
 	return x + y
 }
 
+func Divide(w http.ResponseWriter, r *http.Request) {
+	x := 100.2
+	y := 10.3
+	f, err := divideValues(x, y)
+
+	if err != nil {
+		fmt.Fprintf(w, "Cannot divide by 0")
+		return
+	}
+
+	fmt.Fprintf(w, "%.2f divided by %.2f is %.2f", x, y, f)
+}
+
+func divideValues(x, y float64) (float64, error) {
+
+	if y <= 0 {
+		err := errors.New("cannot divide by zero")
+		return 0, err
+	}
+
+	result := x / y
+	return result, nil
+}
+
 func main() {
 
 	http.HandleFunc("/", Home)
 	http.HandleFunc("/about", About)
+	http.HandleFunc("/divide", Divide)
 
 	fmt.Printf("Starting application on port %s", portNumber)
 	http.ListenAndServe(portNumber, nil)
