d7849b27a130530d72ae4b36b575a11660dc98ed
Tom Baer <thomasbaehren@gmail.com>
2024-09-14 20:31:27 +0200

    restructure how template is cached

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -2,17 +2,37 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net/http"
 
+	"github.com/TomDBaer/basic-go-webapp/pkg/config"
 	"github.com/TomDBaer/basic-go-webapp/pkg/handlers"
+	"github.com/TomDBaer/basic-go-webapp/pkg/render"
 )
 
 const portNumber = ":8080"
 
+// main is the main application function
 func main() {
 
-	http.HandleFunc("/", handlers.Home)
-	http.HandleFunc("/about", handlers.About)
+	// Hier wird die config geladen ("pkg/config")
+	var app config.AppConfig
+
+	// tt := render.CreateTemplateCache()
+
+	templateCacheAd, err := render.CreateTemplateCacheAdvanced()
+	if err != nil {
+		log.Fatal("cannot create template cache")
+	}
+	app.TemplateCacheAdvanced = templateCacheAd
+
+	repo := handlers.NewRepo(&app)
+	handlers.NewHandlers(repo)
+
+	render.NewTemplates(&app)
+
+	http.HandleFunc("/", handlers.Repo.Home)
+	http.HandleFunc("/about", handlers.Repo.About)
 	http.HandleFunc("/divide", handlers.Divide)
 
 	fmt.Printf("Starting application on port %s\n", portNumber)
